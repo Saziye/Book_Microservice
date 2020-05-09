73c0e2c9d3bcb9425856b8dd4a5b8526e34bb06b
Saz <saziye@gmail.com>
2020-05-10 01:57:21 +0300

    Creating model for books

--- app/Book.php
+++ app/Book.php
@@ -8,25 +8,18 @@ use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class Book extends Model 
 {
-    use Authenticatable, Authorizable;
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email',
+        'title',
+        'description',
+        'price',
+        'author_id',
     ];
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-    ];
 }
